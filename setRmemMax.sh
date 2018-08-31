c128de462986240e5b4287e882a1f6c1b8642550
wuyuepku <wuyue16@pku.edu.cn>
2018-08-31 13:30:36 -0500

    update socket buf size

--- setRmemMax.sh
+++ setRmemMax.sh
@@ -3,5 +3,6 @@
 # use this file to set rmem_max parameter of Ubuntu16.04 system, since it's 208kb by default
 
 # 16MB
-sudo sysctl -w net.core.rmem_max=16777216
-
+# sudo sysctl -w net.core.rmem_max=16777216
+# 67MB
+sudo sysctl -w net.core.rmem_max=67108864
