9206148d5fd2c27d4b32fe5832a02a61b05035c3
weichen <pkuweichen@gmail.com>
2019-05-07 16:16:12 +0800

    5.7.1

--- HSRNet/matlab_process/csiplot.m
+++ HSRNet/matlab_process/csiplot.m
@@ -1,5 +1,6 @@
 clear;
-rxdir=['../rxdata/5.5/5.5.1/']; filenum=80;
+%rxdir=['../rxdata/5.5/5.5.0/']; filenum=80; ref_port = 8;
+rxdir=['../rxdata/5.5/5.5.1/']; filenum=80; ref_port = 8;
 unwrap_able = 1;
 
 trial_check_list = [];
@@ -17,7 +18,7 @@ for i=1:repeat_num
     csi_idx(1+29*(i-1):29*i) = (1+59*(i-1):29+59*(i-1));
 end
 
-for i=1:filenum
+for i=76:filenum
     csifile = [rxdir 'epoch' int2str(i-1) '/csi.mat'];
     disp(csifile);
     load(csifile);
@@ -44,10 +45,39 @@ for i=1:filenum
     csi_list = h_all_est(:,csi_idx,port_able);
     csi_num = size(csi_list,2);
     trial_csi_list(:,1+15*(i-1):15*i,port_able) = csi_list(:,1:29:end,:);
-%   trial_csi_list(:,1+csi_num*(filenum-1):csi_num*filenum,port_able) = csi_list; 
-%     csi_delta_list =  csi_list(:,:,1)./csi_list(:,:,2);
-%     unwrapped_angle = unwrap(unwrap(angle(csi_delta_list),[],2),[],1);
-%     mesh(unwrapped_angle);
+
+    %display(snr_list)
+    port1 = 5; port2 = 6;
+    plot_csi = csi_list(:,:,port1)./csi_list(:,:,port2).*abs(csi_list(:,:,port2));
+    figure; mesh(angle(plot_csi));
+    % CIR and doppler spread
+    CIR=ifft(plot_csi,[],1); % CIR time, symbol time
+    figure; mesh(abs(fftshift(CIR))); title('CIR');
+    PDP = mean(CIR.^2,2); % CIR time
+    figure; plot(fftshift((abs(PDP(:,:))))); title('PDP');
+    DS = fft(CIR,[],2); % CIR time, spead freq
+    figure; mesh(abs(fftshift(fftshift(DS(:,:),1),2))); title('Doppler Spread');
+    
+    close all;
 end
 
+
+% rxbf_snr
 plot(trial_bf_snr_list); hold on; plot(trial_max_snr_list); plot(trial_snr_list); legend('bf_snr','max_snr');
+
+% ber vs snr
+figure; subplot(2,1,1); semilogy(trial_ber_list); title('ber');
+    subplot(2,1,2); plot(trial_snr_list); title('snr');
+
+% cfo vs snr
+figure; subplot(2,1,1); plot(trial_cfo_list); title('cfo');
+    subplot(2,1,2); plot(trial_snr_list); title('snr');
+
+% csi plot
+trial_delta_csi_list = [];
+for i=1:size(trial_csi_list,3)
+    if (i==ref_port) continue; end
+    trial_delta_csi_list(:,:,i) = trial_csi_list(:,:,i)./trial_csi_list(:,:,ref_port);
+end
+
+save([rxdir 'result.mat']);
